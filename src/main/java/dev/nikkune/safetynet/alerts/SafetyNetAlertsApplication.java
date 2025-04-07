bc229e3fe0d675c83f09ac042274dee5dd6e1fd6
Nikkune <dev.2tvhg@passmail.net>
2025-04-07 15:31:13 +0200

    MVC Refactor

--- src/main/java/dev/nikkune/safetynet/alerts/SafetyNetAlertsApplication.java
+++ src/main/java/dev/nikkune/safetynet/alerts/SafetyNetAlertsApplication.java
@@ -1,4 +1,4 @@
-package dev.nikkune.safetynet_alerts;
+package dev.nikkune.safetynet.alerts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/dev/nikkune/safetynet/alerts/SafetyNetAlertsApplicationTests.java
+++ src/test/java/dev/nikkune/safetynet/alerts/SafetyNetAlertsApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.nikkune.safetynet_alerts;
+package dev.nikkune.safetynet.alerts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
