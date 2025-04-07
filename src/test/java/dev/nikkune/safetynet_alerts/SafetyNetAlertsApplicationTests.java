7929e9f5e17301e66cf7d6aab6957060111d9c6f
Nikkune <dev.2tvhg@passmail.net>
2025-04-07 14:39:25 +0200

    Initial commit

---
+++ src/test/java/dev/nikkune/safetynet_alerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nikkune.safetynet_alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
